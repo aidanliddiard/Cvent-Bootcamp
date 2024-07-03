7d46b886f105207ee14c2a9e63ed6e850c2aa0ef
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-07-03 07:12:20 -0700

    working on blackjack

--- week12/OOP/BlackJack/src/main/java/org/example/Game.java
+++ week12/OOP/BlackJack/src/main/java/org/example/Game.java
@@ -1,4 +1,24 @@
-package org.example;
-
-public class Game {
-}
+//package org.example;
+//
+//public class Game {
+//
+////    public
+////    Deck deck = new Deck();
+////    deck.shuffle();
+////
+////    for (Player player : players) {
+////
+////    }
+////    public void calculateWinner() {
+////
+////    }
+//
+////    public Hand startGame() {
+////
+////    }
+//    public Card dealOneCard() {
+//        Card card = deck.deal();
+//    }
+//
+//
+//}
